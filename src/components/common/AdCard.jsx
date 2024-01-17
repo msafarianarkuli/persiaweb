cf441b5268664e2cec9948a8a99019ec82bad05c
maryam safari <m.safari.anarkuli@gmail.com>
2024-01-17 10:33:59 +0330

    image performance

--- src/components/common/AdCard.jsx
+++ src/components/common/AdCard.jsx
@@ -5,7 +5,7 @@ function AdCard({ item }) {
   const { title, description } = item;
   return (
     <div className='flex p-2 bg-white rounded-md shadow-[0px_0px_9px_-7px_rgba(0,0,0,0.5)]'>
-      <Image width={110} height={110} src={PersiaWeb} alt='' />
+      <Image width={110} height={110} src={PersiaWeb} alt='' priority />
       <div className='flex flex-col items-start justify-center gap-y-4'>
         <p className='text-text-0'>{title}</p>
         <p className='text-text-100'>{description}</p>
