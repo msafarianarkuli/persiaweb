aa8051c404cd028b4e948a3117e43f37e4e65de7
maryam safari <m.safari.anarkuli@gmail.com>
2024-01-21 08:41:09 +0330

    create bookmark and ads content

--- src/views/main/bookmarks/index.jsx
+++ src/views/main/bookmarks/index.jsx
@@ -1,5 +1,14 @@
+import H1 from "@/components/ui/heading/H1";
+import Container from "../home/components/Container";
+import { ads } from "@/utils/fakeData";
+
 function Bookmarks() {
-  return <div></div>;
+  return (
+    <main>
+      <H1>نشان شده ها</H1>
+      <Container items={ads} />
+    </main>
+  );
 }
 
 export default Bookmarks;
--- src/views/main/myAdvertisements/index.jsx
+++ src/views/main/myAdvertisements/index.jsx
@@ -1,5 +1,14 @@
+import H1 from "@/components/ui/heading/H1";
+import Container from "../home/components/Container";
+import { ads } from "@/utils/fakeData";
+
 function MyAdvertisements() {
-  return <div></div>;
+  return (
+    <main>
+      <H1>آگهی های من</H1>
+      <Container items={ads} />
+    </main>
+  );
 }
 
 export default MyAdvertisements;
