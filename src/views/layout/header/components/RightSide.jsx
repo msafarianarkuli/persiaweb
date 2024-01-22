85ddaf6a496e197e16f86c9e49481a7b9bc956a4
maryam safari <m.safari.anarkuli@gmail.com>
2024-01-22 18:08:17 +0330

    add header image

--- src/views/layout/header/components/RightSide.jsx
+++ src/views/layout/header/components/RightSide.jsx
@@ -1,9 +1,10 @@
 "use client";
-import SearchDropDown from "@/components/ui/buttons/SearchDropDown";
-import SearchInput from "@/components/ui/inputs/SearchInput";
-import { useEffect, useRef, useState } from "react";
+import Image from "next/image";
 import { BiSolidBriefcaseAlt2 } from "react-icons/bi";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import SearchDropDown from "@/components/ui/buttons/SearchDropDown";
+import SearchInput from "@/components/ui/inputs/SearchInput";
+import logo from "@/assets/images/logo-primary.png";
 
 const locations = [
   { id: 1, title: "اصفهان" },
@@ -38,6 +39,7 @@ const jobs = [
 function RightSide() {
   return (
     <div className='flex justify-between w-full lg:w-auto gap-x-2'>
+      <Image src={logo} alt='' width={60} />
       <div className='hidden lg:flex gap-x-2'>
         <SearchDropDown
           title='همه جا'
