8d21b62b8b8b4c7356befd87ecc965a013326372
maryam safari <m.safari.anarkuli@gmail.com>
2024-01-23 11:31:29 +0330

    create upload components

---
+++ src/components/ui/picture/Upload.jsx
@@ -0,0 +1,57 @@
+"use client";
+import { useRef, useState } from "react";
+import { FaUser } from "react-icons/fa";
+
+const Upload = () => {
+  const inputRef = useRef(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleUpload = (e) => {
+    if (e?.target?.files?.[0]) {
+      const file = e.target.files[0];
+      setSelectedImage(file);
+    }
+  };
+
+  const handleRemoveProfile = () => {
+    setSelectedImage(null);
+  };
+
+  return (
+    <div className='flex flex-col items-center gap-y-4 text-center'>
+      {selectedImage ? (
+        <img
+          src={URL.createObjectURL(selectedImage)}
+          alt='Profile Picture'
+          className='w-[90px] h-[90px] rounded-full object-cover'
+        />
+      ) : (
+        <div
+          className='w-fit h-fit rounded-full p-5 bg-[#f6f6f6] cursor-pointer'
+          onClick={() => {
+            inputRef.current?.click();
+          }}>
+          <FaUser className='text-3xl text-text-300' />
+        </div>
+      )}
+      <h4
+        className='text-sm/4 text-text-300 cursor-pointer'
+        onClick={handleRemoveProfile}>
+        {selectedImage ? (
+          <span className='text-red-400'>Remove Profile Picture</span>
+        ) : (
+          <span>Profile Picture (Optional)</span>
+        )}
+      </h4>
+      <input
+        type='file'
+        ref={inputRef}
+        className='hidden'
+        onChange={(event) => handleUpload(event)}
+        accept={".jpg, .jpeg, .png"}
+      />
+    </div>
+  );
+};
+
+export default Upload;
--- src/views/main/advertisementRegistration/components/AdRegistrationForm.jsx
+++ src/views/main/advertisementRegistration/components/AdRegistrationForm.jsx
@@ -7,6 +7,7 @@ import { Advantages, age, education, english, gender } from "@/utils/constants";
 import Radio from "@/components/ui/inputs/Radio";
 import Textarea from "@/components/ui/inputs/Textarea";
 import Checkbox from "@/components/ui/inputs/Checkbox";
+import Upload from "@/components/ui/picture/Upload";
 
 function AdRegistrationForm() {
   const initialValues = {
@@ -19,91 +20,101 @@ function AdRegistrationForm() {
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      onSubmit={(values) => console.log(values)}>
-      <Form>
-        <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 mt-6'>
-          <Select
-            label='استان'
-            data={gender}
-            id='state'
-            name='state'
-            defaultValue={gender?.[0]}
-          />
-          <Select
-            label='عنوان شغلی'
-            data={gender}
-            id='state'
-            name='state'
-            defaultValue={gender?.[0]}
-          />
-        </div>
-        <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 mt-6'>
-          <Input type='text' name='industy' id='industy' label='نام شرکت' />
-          <Input type='text' name='age' id='age' label='سن مورد نیاز' />
-        </div>
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6'>
-          <Input
-            type='text'
-            name='experience'
-            id='experience'
-            label='سابقه کار مورد نیاز'
-          />
-          <Input type='text' name='worktime' id='worktime' label='ساعت کاری' />
-          <Select
-            label='میزان حقوق'
-            data={gender}
-            id='income'
-            name='income'
-            defaultValue={gender?.[0]}
-          />
-        </div>
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6'>
-          <div className='flex gap-x-2'>
-            <span className='me-4'>جنسیت مورد نیاز</span>
-            {gender.map((opt, index) => (
-              <Checkbox
-                label={opt.label}
-                key={index}
-                name='gender'
-                value={opt.value}
-              />
-            ))}
+    <>
+      <Upload />
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => console.log(values)}>
+        <Form>
+          <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 mt-6'>
+            <Select
+              label='استان'
+              data={gender}
+              id='state'
+              name='state'
+              defaultValue={gender?.[0]}
+            />
+            <Select
+              label='عنوان شغلی'
+              data={gender}
+              id='state'
+              name='state'
+              defaultValue={gender?.[0]}
+            />
           </div>
-        </div>
-        <div className='grid grid-cols-1 gap-6 mt-6'>
-          <div className='flex'>
-            <span className='me-4'>میزان تحصیلات مورد نیاز</span>
-            {education.map((opt, index) => (
-              <Radio
-                label={opt.label}
-                key={index}
-                name='education'
-                value={opt.value}
-              />
-            ))}
+          <div className='grid grid-cols-1 lg:grid-cols-2 gap-6 mt-6'>
+            <Input type='text' name='industy' id='industy' label='نام شرکت' />
+            <Input type='text' name='age' id='age' label='سن مورد نیاز' />
           </div>
-        </div>
-        <div className='grid grid-cols-1 gap-6 mt-6'>
-          <div className='flex'>
-            <span className='me-4'>مزایا</span>
-            {Advantages.map((opt, index) => (
-              <Checkbox
-                label={opt.label}
-                key={index}
-                name='advantages'
-                value={opt.value}
-              />
-            ))}
+          <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6'>
+            <Input
+              type='text'
+              name='experience'
+              id='experience'
+              label='سابقه کار مورد نیاز'
+            />
+            <Input
+              type='text'
+              name='worktime'
+              id='worktime'
+              label='ساعت کاری'
+            />
+            <Select
+              label='میزان حقوق'
+              data={gender}
+              id='income'
+              name='income'
+              defaultValue={gender?.[0]}
+            />
           </div>
-        </div>
-        <Textarea type='text' name='address' id='address' label='محل کار' />
-        <Button type='submit' className='bg-primary-green text-white mt-4 mb-8'>
-          ارسال رزومه
-        </Button>
-      </Form>
-    </Formik>
+          <div className='grid grid-cols-1 lg:grid-cols-3 gap-6 mt-6'>
+            <div className='flex gap-x-2'>
+              <span className='me-4'>جنسیت مورد نیاز</span>
+              {gender.map((opt, index) => (
+                <Checkbox
+                  label={opt.label}
+                  key={index}
+                  name='gender'
+                  value={opt.value}
+                />
+              ))}
+            </div>
+          </div>
+          <div className='grid grid-cols-1 gap-6 mt-6'>
+            <div className='flex'>
+              <span className='me-4'>میزان تحصیلات مورد نیاز</span>
+              {education.map((opt, index) => (
+                <Radio
+                  label={opt.label}
+                  key={index}
+                  name='education'
+                  value={opt.value}
+                />
+              ))}
+            </div>
+          </div>
+          <div className='grid grid-cols-1 gap-6 mt-6'>
+            <div className='flex'>
+              <span className='me-4'>مزایا</span>
+              {Advantages.map((opt, index) => (
+                <Checkbox
+                  label={opt.label}
+                  key={index}
+                  name='advantages'
+                  value={opt.value}
+                />
+              ))}
+            </div>
+          </div>
+          <Textarea type='text' name='address' id='address' label='محل کار' />
+          <Button
+            type='submit'
+            className='bg-primary-green text-white mt-4 mb-8'>
+            ارسال رزومه
+          </Button>
+        </Form>
+      </Formik>
+    </>
   );
 }
 
