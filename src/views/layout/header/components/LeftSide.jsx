2954d27a7a7692df775110ec159ed54875625440
maryam safari <m.safari.anarkuli@gmail.com>
2024-01-28 13:28:09 +0330

    change notified received resume button style

--- src/views/layout/header/components/LeftSide.jsx
+++ src/views/layout/header/components/LeftSide.jsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 import { FaPlus, FaUser } from "react-icons/fa";
 import { AiOutlineIdcard } from "react-icons/ai";
-import Link from "next/link";
+import { LuBellRing } from "react-icons/lu";
 
 function LeftSide() {
+  const notif = true;
   return (
     <div className='hidden lg:flex gap-x-4 items-center '>
       <Link href='profile' className='ouline-white-button'>
@@ -14,10 +16,23 @@ function LeftSide() {
         className='primary-button font-medium'>
         <FaPlus /> ثبت آگهی
       </Link>
-      <Link href='resumes-received' className='secondary-button font-medium'>
-        <AiOutlineIdcard className='text-lg' />
-        رزومه های دریافتی
-      </Link>
+      <div className='relative'>
+        <Link
+          href='resumes-received'
+          className={`secondary-button font-medium ${
+            notif
+              ? "border border-red-500 bg-white hover:bg-bg-100 hover:text-text-200"
+              : ""
+          }`}>
+          <AiOutlineIdcard className='text-lg' />
+          رزومه های دریافتی
+        </Link>
+        {notif && (
+          <span className='flex justify-center items-center absolute -top-2 -right-2 bg-red-500 text-white w-[20px] h-[20px] rounded-full'>
+            <LuBellRing />
+          </span>
+        )}
+      </div>
     </div>
   );
 }
