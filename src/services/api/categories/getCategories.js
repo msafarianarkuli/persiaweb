1e4cc3cec64c5639316287844f9b19428acd6350
maryam safari <m.safari.anarkuli@gmail.com>
2024-03-25 12:53:26 +0330

    no page to category

--- src/services/api/categories/getCategories.js
+++ src/services/api/categories/getCategories.js
@@ -3,7 +3,7 @@ import { API } from "@/services/const";
 
 const getCategories = async ({ search }) => {
   const data = await Http.get(
-    `${API.CATEGORIES}${search ? `?search=${search}` : ""}`
+    `${API.CATEGORIES}${search ? `?search=${search}` : ""}?no_page=1`
   );
   return data?.data;
 };
